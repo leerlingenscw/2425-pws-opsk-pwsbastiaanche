201b9bde8ffb03a79684b4431c9f5d087e0b14ec
Che136054 <136054@stanislascollege.net>
2025-09-13 13:34:17 +0000

    Spelfouten gefikst

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ intro_title = "Wanted in Greece!"
 
 intro_texts = [
     "Welcome to our fantastic game! We would like to welcome you to Wanted\nin Greece! This game is about you! A Greek hero who is on his way to his\nnext mission. All of the sudden you hear something in the bushes, you look\nbehind the bushes and all of the sudden you see a monster!\nYou have been ambushed!\n\n\n\nPress Enter to continue ",
-    "You will be able to choose your character in a seconnd, but first let's\ntalk about the controls. You can use W, to move up, A to go left,\nS to go down and D to go right. You will have 3 hearts, everytime you touch a\nmonster you will lose a heart. You also have a weapon that\ncirkels around you. If your weapon touches a monster you will get 10 points\nand 1 coin. With the coins you will be able to buy heart, other weapons and\nmore weapons in the shop between waves.\nIf you have a score of 250 you win!\nPress Enter to continue ",
-    "We have made 3 levels for you: Easy, Medium and Hard. You need to beat\nthe easier level before you can play the other one. At Level Hard\nyou will have a different kind of monster tht is fast so watch out!I hope you\nenjoy the game we do.\nlet's get right into choosing our character!\n\n\nPress Enter to continue"
+    "You will be able to choose your character in a second, but first let's\ntalk about the controls. You can use W, to move up, A to go left,\nS to go down and D to go right. You will have 3 hearts, everytime you touch a\nmonster you will lose a heart. You also have a weapon that\ncirkels around you. If your weapon touches a monster you will get 10 points\nand 1 coin. With the coins you will be able to buy hearts, other weapons and\nmore weapons in the shop between waves.\nIf you have a score of 250 you win!\nPress Enter to continue ",
+    "We have made 3 levels for you: Easy, Medium and Hard. You need to beat\nthe easier level before you can play the other one. At Level Hard\nyou will have a different kind of monster that is faster so watch out! I hope you\nenjoy the game we do.\nlet's get right into choosing our character!\n\n\nPress Enter to continue"
 ]
 
 current_text_index = 0
